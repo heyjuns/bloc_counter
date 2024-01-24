3f2b11542a090c2816d09be93c94fe0adf62067f
Juanda <heyjuns@icloud.com>
2024-01-24 22:58:54 +0700

    init repo

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+import 'package:bloc_counter/app.dart';
+import 'package:bloc_counter/counter_observer.dart';
+
+void main() {
+  Bloc.observer = const CounterObserver();
+  runApp(const CounterApp());
+}
